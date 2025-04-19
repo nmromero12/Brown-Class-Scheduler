852e66a5713f3edff40d89ff38423a3f94a2f349
Nicholas Romero <nicholas_romero@brown.edu>
2025-04-18 21:42:57 -0400

    Feat: Initial setup

---
+++ brown-course-scheduler/src/main/java/dev/nicoanderic/brown_course_scheduler/BrownCourseSchedulerApplication.java
@@ -0,0 +1,13 @@
+package dev.nicoanderic.brown_course_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrownCourseSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrownCourseSchedulerApplication.class, args);
+	}
+
+}
---
+++ brown-course-scheduler/src/test/java/dev/nicoanderic/brown_course_scheduler/BrownCourseSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nicoanderic.brown_course_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrownCourseSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
