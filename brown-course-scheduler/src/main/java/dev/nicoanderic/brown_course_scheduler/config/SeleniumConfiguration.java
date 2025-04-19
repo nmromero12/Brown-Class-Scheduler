c3a25890b4fb3dae0f496ffdc3a72b4e2e738354
Nicholas Romero <nicholas_romero@brown.edu>
2025-04-18 21:59:41 -0400

    Feat: Added chrome driver web configuration

---
+++ brown-course-scheduler/src/main/java/dev/nicoanderic/brown_course_scheduler/config/SeleniumConfiguration.java
@@ -0,0 +1,24 @@
+package dev.nicoanderic.brown_course_scheduler.config;
+
+import jakarta.annotation.PostConstruct;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SeleniumConfiguration {
+  @PostConstruct
+  void postConstruct() {
+    System.setProperty("webdriver.chrome.driver", "/home/nromero/chromedriver-linux64/chromedriver");
+  }
+  @Bean
+  public ChromeDriver driver()
+
+  {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--headless=new");
+    return new ChromeDriver(options);
+  }
+
+}
