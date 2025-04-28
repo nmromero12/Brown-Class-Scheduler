0ff228782fd649341478487ce64a73ee1b672b71
Nicholas Romero <nicholas_romero@brown.edu>
2025-04-28 11:34:46 -0400

    Feat: Database fetching from frontend

--- client/src/components/SearchCourse.tsx
+++ client/src/components/SearchCourse.tsx
@@ -22,8 +22,24 @@ export function SearchCourse() {
 
     const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         setSearchCode(event.target.value);
-        
     };
+
+
+    async function fetchCourses() {
+        try {
+            const response = await fetch(`http://localhost:8080/api/courses/code/${searchCode}`)
+            if (!response.ok) throw new Error("Failed fetching data");
+            const data = await response.json()
+            if (data.result == "success") {
+                setCourses(data.courses);
+                console.log(data.courses);
+            }
+                
+        } catch (error) {
+            console.error("fetch error:", error);
+        }
+
+    }
 
 
     
@@ -42,8 +58,27 @@ export function SearchCourse() {
             value={searchCode}
             onChange={handleInputChange}
             placeholder="Enter Course Code"></input>
-            <button>Submit</button>
+            <button onClick={fetchCourses}>Submit</button>
+
+            <div>
+      {courses && courses.length > 0 ? (
+        <ul>
+          {courses.map((course) => (
+            <li key={course.id}>
+              <strong>{course.courseCode}</strong>: {course.courseName} — Section {course.section}<br />
+              Class Time: {course.classTime}, Exam Time: {course.examTime}, CRN: {course.crn}
+            </li>
+          ))}
+        </ul>
+      ) : courses === null ? (
+        <p>Enter a course code and click submit.</p>
+      ) : (
+        <p>No courses found.</p>
+      )}
+    </div>
+            
         </div>
+        
 
 
     )
