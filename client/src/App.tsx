44fa05c047010cd7b42ab6b4e71e0583dcb1c505
Nicholas Romero <nicholas_romero@brown.edu>
2025-04-29 13:33:00 -0400

    Feat: Setup routing for navigation of the web application

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,19 @@
 import './App.css'
 import Navbar from './components/Navbar.tsx'
 import { SearchCourse } from './components/SearchCourse.tsx'
+import {Routes, Route} from "react-router-dom"
 
 function App() {
 
   return (
   <>
-  <Navbar>
+  <Navbar/>
+    <Routes>
+      <Route path="/search" element={<SearchCourse/>}></Route>
+      <Route path="/calender"></Route>
+    </Routes>
     
-  </Navbar>
-  <SearchCourse></SearchCourse>
+
   </>
   )
 }
