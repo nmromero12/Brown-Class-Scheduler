5b9c99397c123e4004028ad5cd5fece5daa9b6e1
wangeric027 <wang.eric027@gmail.com>
2025-05-02 17:45:43 -0400

    idk

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,5 +1,3 @@
-
-
 export default function Navbar() {
     return (
       <nav className="bg-gray-900 text-white px-4 py-3 shadow-md">
@@ -19,20 +17,14 @@ export default function Navbar() {
               <li>
                 <a href="#" className="px-3 py-1 bg-gray-800 rounded text-white">Dashboard</a>
               </li>
-              <li><a href="#" className="hover:text-white">Team</a></li>
-              <li><a href="#" className="hover:text-white">Projects</a></li>
+              <li><a href="#" className="hover:text-white">Friends</a></li>
+              <li><a href="#" className="hover:text-white">Cart</a></li>
               <li><a href="#" className="hover:text-white">Calendar</a></li>
             </ul>
           </div>
   
           {/* Right: Icons */}
           <div className="flex items-center space-x-4">
-            {/* Bell Icon */}
-            <button className="p-2 rounded-full hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-white">
-              <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V4a2 2 0 10-4 0v1.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0a3 3 0 01-6 0h6z" />
-              </svg>
-            </button>
   
             {/* Profile Avatar (placeholder) */}
             <img
