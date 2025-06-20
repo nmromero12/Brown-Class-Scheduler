73337ac2fa9312b7c1af6db580c9ee8ed30744f6
Nicholas Romero <nicholas_romero@brown.edu>
2025-06-19 21:11:20 -0400

    Feat: Basic Login Page Design

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import "./index.css";
 import App from "./App.tsx";
+import Login from "./components/Login.tsx";
 
+import { initializeApp} from "firebase/app";
 
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
+
+initializeApp(firebaseConfig)
 
 
 
@@ -14,6 +26,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <Router>
       <Routes>
         <Route path="/" element={<App/> } />
+        <Route path="/login" element={<Login/> } />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
